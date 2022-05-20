426fe9626ef3cf3b5fcb6ff82d18434ebedaaa1b
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2022-05-20 22:59:35 +0100

    08 - A Dummy is a Stand-in for the Real Thing (Using Normal Gateway)

--- src/Subscription.php
+++ src/Subscription.php
@@ -4,11 +4,23 @@ namespace App;
 
 class Subscription
 {
+    protected Gateway $gateway;
+
+    /**
+     * @param Gateway $gateway
+     */
+    public function __construct(Gateway $gateway)
+    {
+        $this->gateway = $gateway;
+    }
+    
     public function create(User $user)
     {
         // create the subscription through Stripe.
+        $this->gateway->create();
 
         // Update the local user record.
+        $user->markAsSubscribed();
 
         // Send a welcome email or dispatch event.
     }
--- src/User.php
+++ src/User.php
@@ -6,8 +6,20 @@ class User
 {
     protected $name;
 
+    protected $subscribed = false;
+
     public function __construct($name)
     {
         $this->name = $name;
+    }
+
+    public function isSubscribed(): bool
+    {
+        return $this->subscribed;
+    }
+
+    public function markAsSubscribed()
+    {
+        $this->subscribed = true;
     }
 }
