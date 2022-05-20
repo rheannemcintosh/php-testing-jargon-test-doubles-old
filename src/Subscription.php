244e76c8a6093b77b32cc7524816d7e984f33083
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2022-05-20 22:43:28 +0100

    Setup the basic files

---
+++ src/Subscription.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+class Subscription
+{
+    public function create(User $user)
+    {
+        // create the subscription through Stripe.
+
+        // Update the local user record.
+
+        // Send a welcome email or dispatch event.
+    }
+}
